c5440cb394dfb8f9492154772ceb866e3ddeeb69
robertkajnak <hezirisz@gmail.com>
2018-03-14 19:22:50 +0100

    Modified/added/removed some comments

--- Assignment_4/stich.m
+++ Assignment_4/stich.m
@@ -13,22 +13,20 @@ offset=floor(bt.m*[1;1]+bt.t);
 base = boat2;
 rotated = transform_image(base,bt.m,bt.t);
 
+%calculate the new size for the image
 nsz = round([max(offset(2)+size(rotated,2),size(base,2))-min(offset(2),0),...
        max(offset(1)+size(rotated,1),size(base,1))-min(offset(1),0)]);
 nsz = [nsz(2),nsz(1)];
+
+%overlay the two images
 im = zeros(nsz);
 figure;
 im=overlay(im,boat1,-offset.*(offset<0));
-imshow(im,[]);
 im=overlay(im,rotated,offset.*(offset>0));
 imshow(im,[]);
 
-   
-%figure;
-%imshow(rotated);
-%hold on;
-%imshow(base);d
-   
+%TODO If you know a matlab function for this one, feel free to replace it. 
+%The one I found was kinda complicated
 function [new_image]=overlay(image1,image2,offset)
     new_image = image1;
     for i=1:size(image2,1)
