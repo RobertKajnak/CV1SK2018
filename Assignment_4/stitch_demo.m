b22ad7d2caef729f263ef58c252d48dcc99d044f
robertkajnak <hezirisz@gmail.com>
2018-03-16 11:16:49 +0100

    Original images now plotted for the demo

--- Assignment_4/stitch_demo.m
+++ Assignment_4/stitch_demo.m
@@ -1,6 +1,14 @@
 image1 = 'left.jpg';
 image2 = 'right.jpg';
 
+figure('name','Original right and left images');
+subplot(1,2,1);
+imshow(image1);
+title('left.jpg');
+subplot(1,2,2);
+imshow(image2);
+title('right.jpg');
+
 figure('name','Aligning left.jpg with right.jpg');
 stitched = stitch(image2,image1);
 imshow(stitched);
